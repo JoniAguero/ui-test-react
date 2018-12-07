3efb56c637fbd3ed72e7b7eb4e573a43d9f43077
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-07 13:27:51 -0300

    Map Component

--- src/components/DiagramMap/DiagramMap.js
+++ src/components/DiagramMap/DiagramMap.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react'
 import './DiagramMap.css';
 import Diagram from './Diagram/Diagram';
+import Map from './Map/Map';
 export class DiagramMap extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: 'diagram'
+        };
+    }
+
+    showDiagram = (e) => {
+        e.preventDefault();
+        console.log('showDiagram.');
+        this.setState({
+            show: 'diagram'
+        })
+    }
+
+    showMap = (e) => {
+        e.preventDefault();
+        console.log('showMap.');
+        this.setState({
+            show: 'map'
+        })
+    }
   render() {
     return (
       <div className="diagram-map-container">
         <div className="d-flex">
             <div className="mr-auto p-2 title-diagram"></div>
             <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                <label className="btn btn-diagram btn-light btn-sm active">
-                    <input type="radio" name="options" id="option1" autoComplete="off"/> Diagram
+                <label className="btn btn-diagram btn-light btn-sm active" onClick={this.showDiagram}>
+                    <input type = "radio"
+                    name = "options"
+                    id = "option1"
+                    autoComplete = "off"
+                    /> Diagram
                 </label>
-                <label className="btn btn-map btn-light btn-sm">
+                <label className="btn btn-map btn-light btn-sm" onClick={this.showMap}>
                     <input type="radio" name="options" id="option2" autoComplete="off" /> Map
                 </label>
             </div>
         </div>
         <div className="diagram-text">Text</div>
         <div className="diagram-container">
-            <Diagram />
+            {
+                this.state.show === 'diagram' ? <Diagram /> : <Map />
+            } 
         </div>
       </div>
     )
---
+++ src/components/DiagramMap/Map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Map.css';
+const Map = () => {
+  return (
+    <div className="container-map">
+      
+    </div>
+  )
+}
+
+export default Map
