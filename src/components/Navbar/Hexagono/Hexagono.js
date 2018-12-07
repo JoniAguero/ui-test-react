3ff053e189d053b244029bb30a46d7b088957f7a
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-07 00:49:19 -0300

    Hexagono + navbar

---
+++ src/components/Navbar/Hexagono/Hexagono.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Hexagono.css';
+export default function Hexagono() {
+  return (
+    <div className="hexagon"></div>
+  )
+}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,54 @@
 import React, { Component } from 'react'
+import './Navbar.css';
+import Hexagono from './Hexagono/Hexagono';
 
 export class Navbar extends Component {
   render() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white">
-            <a className="navbar-brand" href="/">Dashboard</a>
+            <Hexagono />
+            <a className="dashboard" href="/">Dashboard</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Billing</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Billing</a>
                     </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Network</a>
+                    <li className="nav-item">
+                        <a className="nav-link black" href="/">Network</a>
                     </li>
                 </ul>
+                <div className="form-inline my-2 my-lg-0">
+                    <div className="btn-group">
+                        <button type="button" className="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Action
+                        </button>
+                        <div className="dropdown-menu">
+                            <a className="dropdown-item" href="/">Action</a>
+                            <a className="dropdown-item" href="/">Another action</a>
+                            <a className="dropdown-item" href="/">Something else here</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="/">Separated link</a>
+                        </div>
+                        </div>
+                </div>
             </div>
         </nav>
     )
