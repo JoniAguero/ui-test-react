6450ea51e3a59932edef0d49737b35cc296cb4c2
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-07 00:12:55 -0300

    Navbar

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from '../Navbar/Navbar';
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
       </div>
     )
   }
