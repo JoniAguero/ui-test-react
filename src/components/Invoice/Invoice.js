7135e0af29e48e84c315b1f87fe4ea95d1f9c9fe
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-07 08:24:34 -0300

    Invoice Grid List

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -6,7 +6,16 @@ export class Invoice extends Component {
       <div className="invoice-container">
         <div className="invoice-title">
             <h4 className="title">Invoice</h4>
-            <span className="span-title">S03834754</span>
+            <span className="span-title" data-toggle="tooltip" data-placement="top" title="Invoice Number">S03834754</span>
+        </div>
+        <div className="grid-invoice">
+          <div className="grid-invoice-title">
+            <p>INVOICE - REVISION</p>
+          </div>
+          <div className="grid-invoice-details">
+            <p className="details-item">Invoice #</p>
+            <p className="details-data">33-0092kk00</p>
+          </div>
         </div>
       </div>
     )
