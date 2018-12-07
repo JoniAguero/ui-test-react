057c47922682478353bb96b831d962132aab3630
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-07 01:17:59 -0300

    Dashboard Grid List

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar';
 
+import './Dashboard.css';
 export class Dashboard extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar /> 
+        <div className="dashboard-container">
+          <div>
+            <div className="grid-container">
+              <div className="invoice">
+
+              </div>
+              <div className="diagram">
+
+              </div>
+              <div className="overview">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
